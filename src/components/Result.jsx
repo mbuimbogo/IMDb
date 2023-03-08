d05f577c943254c04837e35f0b18fa9f805e194e
mbuimbogo <mbuimbogo99@gmail.com>
2023-03-09 01:46:45 +0300

    make the cards responsive

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,7 +3,7 @@ import Card from './Card'
 
 export default function Result({results}) {
   return (
-    <div>
+    <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-5 max-w-6xl mx-auto py-4'>
       {results.map((result)=>(
         <Card key={result.id} result={result}/>
       ))}
