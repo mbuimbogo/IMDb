55ab98e9774d573ab4594c1ccc1fe076557c7b53
mbuimbogo <mbuimbogo99@gmail.com>
2023-03-09 01:55:04 +0300

    style up the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Card({result}) {
   return (
-    <div>
-      {result.original_title}
+    <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 m-2 transition-shadow duration-200'>
+      {result.id}
     </div>
   )
 }
