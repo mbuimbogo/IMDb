5e4b4b45e146a4f9a81748d5cbc5bfa590641166
mbuimbogo <mbuimbogo99@gmail.com>
2023-03-09 11:29:19 +0300

    create a form, implement handle submit and handle its state

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,24 @@
+"use client"
+import React, { useState } from 'react'
+import {  useRouter } from "next/navigation"
+
+export default function SearchBox() {
+const [search, setSearch]=useState("")
+const router = useRouter();
+
+function handleSubmit(e){
+    e.preventDefault();
+    if(!search)return;
+    router.push(`/search/${search}`)
+
+}
+
+  return (
+    <form onSubmit={handleSubmit}
+    className='flex max-w-6xl mx-auto justify-between items-center px-5'>
+      <input onChange={(e)=> setSearch(e.target.value)} 
+      className='w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1' type="text" value={search} placeholder='Search keywords...' />
+      <button disabled={!search} className="text-amber-700 disabled:text-gray-400" type="submit">Search</button>
+    </form>
+  )
+}
