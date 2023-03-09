1fe29e5d784fdb12ef5ee2928f6680e4f9a01d6b
mbuimbogo <mbuimbogo99@gmail.com>
2023-03-09 03:04:42 +0300

    add border and padding to the card for the small screen sizes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import {FiThumbsUp} from "react-icons/fi"
 
 export default function Card({result}) {
   return (
-    <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 m-2 transition-shadow duration-200 group'>
+    <div className='cursor-pointer border p-2 sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 m-2 transition-shadow duration-200 group'>
       <Link href={`/movie/${result.id}`}>
         <Image 
         src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`}
